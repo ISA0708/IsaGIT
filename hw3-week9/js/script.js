dc494c63e5e19e9477aafc649e565d2a29516c90
ISA0708 <gabbasislam59@gmail.com>
2024-10-30 20:54:25 +0500

    hw2 week9

--- hw3-week9/js/script.js
+++ hw3-week9/js/script.js
@@ -19,3 +19,14 @@ function calculate() {
         display.value = 'Ошибка';
     }
 }
+
+let name = "Ислам"; // Типі: string
+let age = 16; //Типі: number
+let isStudent = true; // Типі: boolean
+let ageAsString = "16"; //Типі: string
+
+
+console.log("Аты:", name, "| Типі:", typeof name);
+console.log("Жасы:", age, "| Типі:", typeof age);
+console.log("Студент пе:", isStudent, "| Типі:", typeof isStudent);
+console.log("Жасы(жол ретінде):" , age, "| Типі:" , typeof ageAsString);
