b509dd3a3fa8fbc3b814444b92d0f7c211df5f8d
ISA0708 <gabbasislam59@gmail.com>
2024-10-31 22:32:13 +0500

    HW3 Week 9

---
+++ hw3-week9/HW 3 week 9/js/Hw3.js
@@ -0,0 +1,7 @@
+let number1 = Number(prompt("Бірінші санды енгізіңіз:"));
+let number2 = Number(prompt("үшінші санды енгізіңіз:"));
+let number3 = Number(prompt("Екінші санды енгізіңіз:"));
+
+let divizible = (number1 + number2 + number3) /3;
+
+alert("Енгізілген сандардың орташа арифметикалық мәні: " + divizible);
