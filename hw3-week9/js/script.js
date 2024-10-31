952b67c587f37e649d1eccb2e591ed05d90dbe47
ISA0708 <gabbasislam59@gmail.com>
2024-10-31 17:19:06 +0500

    Class work `2

--- hw3-week9/js/script.js
+++ hw3-week9/js/script.js
@@ -1,24 +1,4 @@
-let display = document.getElementById('display');
-
-function appendCharacter(character) {
-    display.value += character;
-}
-
-function clearDisplay() {
-    display.value = '';
-}
-
-function deleteLast() {
-    display.value = display.value.slice(0, -1);
-}
-
-function calculate() {
-    try {
-        display.value = eval(display.value);
-    } catch {
-        display.value = 'Ошибка';
-    }
-}
+/* 
 
 let name = "Ислам"; // Типі: string
 let age = 16; //Типі: number
@@ -30,3 +10,47 @@ console.log("Аты:", name, "| Типі:", typeof name);
 console.log("Жасы:", age, "| Типі:", typeof age);
 console.log("Студент пе:", isStudent, "| Типі:", typeof isStudent);
 console.log("Жасы(жол ретінде):" , age, "| Типі:" , typeof ageAsString);
+
+
+
+
+let v= "5", s=3
+let sum, minus,divizible,kobeity,mod;
+
+v++;//v = v+1e
+console.log("v++ " + v);
+
+v--;//v = v-1
+console.log("v-- " + v);
+
+v+=5 //v = v + 5
+v-=3 //v = v - 5
+v/=2 //v = v / 5
+v*=3 //v = v * 5
+v%=2 //v = v % 5
+
+//Қосу
+sum = v+s
+
+//азайту
+minus = v-s
+
+//diviz
+divizible = v/s
+
+//Kobeity
+kobeity = v*s
+
+//mod
+mod = v%s
+
+console.log(" v + s = " + sum);
+console.log(" v - s = " + minus);
+console.log(" v / s = " + minus);
+
+alert(" v + s = " + sum + '\n' + " v - s = " + minus + '\n' + "v / s =" + divizible + '\n' + "v * s ="  + kobeity + '\n' + "v % s = "  + mod) */ 
+
+var num = Number(prompt("Введите первое число"))
+var num = Number(prompt("Введите второе число"))
+alert ("Ауданы " + num*num + "\n" + "Периметры " + (num-0+num-0 )*2)
+
