bc381df76042a90989240122dcd4960636cc3d01
ISA0708 <gabbasislam59@gmail.com>
2024-11-04 20:44:00 +0500

    HW4 Week9

--- hw3-week9/HW 4 week 10/HW4 Week10.js
+++ hw3-week9/HW 4 week 10/HW4 Week10.js
@@ -1,19 +1,4 @@
-function checkConditions(age, isStudent, num1, num2) {
-    const discountEligibility = age > 65 || isStudent; // Жеңілдік алуға құқылы
-    const numberRange = (num) => num >= 10 && num <= 20; // 10 мен 20 арасында
-    const negativeCheck = num1 < 0 || num2 < 0; // Біреу теріс
+let age = Number(prompt("Сіз қанша жастасыз?"))
+let student = Number(prompt("Сіз окушысызба? 1-ия, 2-жок"))
 
-    alert("Жеңілдік алуға құқылы: " + discountEligibility); // true немесе false
-    const enteredNumber = parseInt(prompt("Санды енгізіңіз:")); // Пайдаланушыдан сан сұрау
-    alert("Сан 10 мен 20 арасында: " + numberRange(enteredNumber)); // true немесе false
-    alert("Біреу теріс: " + negativeCheck); // true немесе false
-}
-
-// Мысалдар
-const age = 70; // Адамның жасы
-const isStudent = false; // Адам студент пе?
-const number1 = -5; // Бірінші сан
-const number2 = 10; // Екінші сан
-
-checkConditions(age, isStudent, number1, number2);
-
+alert(age>65 || student==1)
