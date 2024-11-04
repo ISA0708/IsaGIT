3bbe6eeaddadf6f5077bae3607c58f31ad361770
ISA0708 <gabbasislam59@gmail.com>
2024-11-04 21:04:11 +0500

    HW4 Week9 3

--- hw3-week9/HW 4 week 10/HW4 Week10.js
+++ hw3-week9/HW 4 week 10/HW4 Week10.js
@@ -1,3 +1,4 @@
-let san = Number(prompt("Введите цифру"))
+let a = Number(prompt("Теріс сандарды тексеру. Бірінші санды енгізіңіз."))
+let b = Number(prompt("Екінші санды жаз"))
 
-alert(san>=10 && san<=20);
+alert(a<0 && b>0 || a>0 && b<0)
